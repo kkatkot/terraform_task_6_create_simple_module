8e7c4d89afbaceb000a391028ae0c10d46fe1618
kkatkot <kateruna.kotyh@gmail.com>
2026-04-30 15:22:37 +0300

    Edit 1

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # Рекомендую додати версію для стабільності
+    }
+  }
+}
+
+provider "azurerm" {
+  features {} # Це обов'язковий блок для провайдера Azure
+}
+
+
 module "resource_group_storage" {
-  source = "app.terraform.io/Mate_Terraform/mate_resource_group_storage/azurerm"
+  source  = "app.terraform.io/Mate_Terraform/resource_group_storage/azurerm"
+  version = "1.0.2"
+
+  resource_group_name  = "my-rg"
+  storage_account_name = "mystorageaccktkat"
+  location             = "West Europe"
 }
